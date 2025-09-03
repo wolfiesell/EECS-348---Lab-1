6f6182e3fb52c2d8c9ca173b8fa7502c1f6b8d1d
Wolfgang Sell <wolfiesell@gmail.com>
2025-09-02 21:09:12 -0500

    Add hello-world program

---
+++ lab1/program.c
@@ -0,0 +1,13 @@
+// credit to https://www.geeksforgeeks.org/c/c-hello-world-program/
+
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // Writing print statement to print hello world
+    printf("hello world");
+
+    return 0;
+}
