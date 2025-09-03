f4a615076ef3319625c8ef65f8d4cdfc2d2b01b4
Wolfgang Sell <wolfiesell@gmail.com>
2025-09-02 21:27:32 -0500

    Replace the hello-world program with the sample C program

--- lab1/program.c
+++ lab1/program.c
@@ -1,13 +1,23 @@
-// credit to https://www.geeksforgeeks.org/c/c-hello-world-program/
-
-// Header file for input output functions
 #include <stdio.h>
-
-// Main function: entry point for execution
+#include <string.h>
 int main() {
-
-    // Writing print statement to print hello world
-    printf("bye world");
-
-    return 0;
+char str[100];
+int i, len;
+printf("Enter a string: ");
+gets(str);
+len = strlen(str);
+for (i = 0; i < len; i++) {
+if (str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - 'a' + 'A';
+}
+}
+printf("The capitalized string is: %s\n", str);
+// Reverse the string.
+for (i = 0, len = strlen(str); i < len / 2; i++) {
+char temp = str[i];
+str[i] = str[len - 1 - i];
+str[len - 1 - i] = temp;
+}
+printf("The reversed string is: %s\n", str);
+return 0;
 }
